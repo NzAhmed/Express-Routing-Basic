c57a3fd92ab15b701e5aab768435468101b40e07
NzAhmed <33981320+NzAhmed@users.noreply.github.com>
2019-08-25 15:56:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+- download and install Node
+- install express
+    >npm i express
+*/
+
 const express = require('express')
 const app = express()
 /*
@@ -21,14 +27,14 @@ app.post('/', function (req, res) {
   res.send('Got a POST request')
 })
 
-// Respond to a PUT request to the /user route:
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user')
+// Respond to a PUT request to the /api/products route:
+app.put('/api/products', function (req, res) {
+  res.send('Got a PUT request at /api/products')
 })
 
-// Respond to a DELETE request to the /user route:
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user')
+// Respond to a DELETE request to the /api/products route:
+app.delete('/api/products', function (req, res) {
+  res.send('Got a DELETE request at /api/products')
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
